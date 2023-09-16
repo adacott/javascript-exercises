f56b267924f8457210b1c46fd9c39bf9e20d5d0d
Adam Cottrill <33773276+adacott@users.noreply.github.com>
2023-09-16 18:38:24 +0000

    Exercise 04 completed with tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+/**
+ * 
+ * @param {array} array
+ * @param  {...any} args 
+ */
+const removeFromArray = function(array, ...args) {
+    for(i = 0; i < args.length; i++){
+        if(array.includes(args[i])){
+            index = array.indexOf(args[i]);
+            array.splice(index, 1);
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
