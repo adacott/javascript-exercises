ad060f3fc0d3e3a179e0cd81fef4cf71c5f83649
Adam Cottrill <33773276+adacott@users.noreply.github.com>
2023-09-16 19:54:12 +0000

    Exercises 06 and 07 completed and tested

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+/**
+ * 
+ * @param {number} year 
+ */
+const leapYears = function(year) {
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 };
+
+// console.log(leapYears(1900));
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+function round(num){
+  num = Math.round(num*10)/10;
+  return num;
+}
+
+// Fahrenheit to celsius is: (fahrenheit - 32) * (5/9)
+const convertToCelsius = function(fahrenheit) {
+  celsius = (fahrenheit - 32) * (5/9);
+  return round(celsius);
 };
 
-const convertToFahrenheit = function() {
+// Celsius to fahrenheit is: celsius * (5/9) + 32
+const convertToFahrenheit = function(celsius) {
+  fahrenheit = (celsius * (9/5)) + 32;
+  return round(fahrenheit);
 };
 
 // Do not edit below this line
