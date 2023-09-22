24d794938704c08d918b1cd50e8977616c259483
Adam Cottrill <33773276+adacott@users.noreply.github.com>
2023-09-22 05:58:48 +0000

    Finished exercises 11 and 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (obj) {
+    const bk = obj.map(objs => objs.title);
+    return bk;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
